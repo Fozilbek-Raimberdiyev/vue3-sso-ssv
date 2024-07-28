038a71b0a116f482a8b76a067876503d6219e891
Fozilbek Raimberdiyev <fl.raimberdiyev@gmail.com>
2024-07-28 18:17:33 +0500

    updatings

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,31 +1,32 @@
 <!-- src/components/Dashboard.vue -->
 
 <template>
+  <div v-if="user">
+    <h1>Dashboard</h1>
+    <p>Welcome, {{ user.name }}</p>
+  </div>
 
-    <div v-if="user">
-      <h1>Dashboard</h1>
-      <p>Welcome, {{ user.name }}</p>
-    </div>
+  <div v-else>
+    <router-link
+      style="
+        margin-right: 10px;
+        background-color: blue;
+        color: white;
+        padding: 5px 15px;
+        border-radius: 5px;
+      "
+      to="/auth/login"
+      >Login</router-link
+    >
+  </div>
+</template>
 
-    <div v-else>
-      <router-link style="margin-right: 10px; background-color: blue; color: white; padding: 5px 15px; border-radius: 5px" to="/auth/login">Login</router-link>
-    </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
-  import { getUserInfo } from '../services/authService.ts';
-  
-  export default {
-    setup() {
-      const user = ref(null);
-  
-      onMounted(async () => {
-        user.value = await getUserInfo();
-      });
-  
-      return { user };
-    }
-  };
-  </script>
-  
+<script setup>
+import { ref, onMounted } from "vue";
+import { getUserInfo } from "../services/authService.ts";
+const user = ref(null);
+
+onMounted(async () => {
+  user.value = await getUserInfo();
+});
+</script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,18 +3,21 @@
 <template>
   <div>
     <h1>Login</h1>
-    <button style="margin-right: 10px; background-color: blue; color: white; padding: 5px 15px; border-radius: 5px" @click="login">Login with SSO</button>
+    <button
+      style="
+        margin-right: 10px;
+        background-color: blue;
+        color: white;
+        padding: 5px 15px;
+        border-radius: 5px;
+      "
+      @click="redirectToSSO"
+    >
+      Login with SSO
+    </button>
   </div>
 </template>
 
-<script>
+<script setup>
 import { redirectToSSO } from "../services/authService.ts";
-
-export default {
-  methods: {
-    login() {
-      redirectToSSO();
-    },
-  },
-};
 </script>
